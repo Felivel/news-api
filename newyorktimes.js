f814e9fe799ae5487a7e6b2159ad2c816a2baea7
Felivel Camilo <felivel.camilo@gmail.com>
2014-04-13 16:23:19 -0400

    coomit from c9

--- newyorktimes.js
+++ newyorktimes.js
@@ -1,7 +1,6 @@
 var     request = require('request'),
         url = require('url');
-
-
+        
 var		NTY_API_KEY = 'c3a958f43f94d1d39a6228faa8fe483b:11:68109101';
 var		response;
 var     parsedArticles;
@@ -9,8 +8,7 @@ var     parsedArticles;
 var getData = function (articles,query,callback) {
 		parsedArticles = articles;
         request.get('http://api.nytimes.com/svc/search/v2/articlesearch.json?q='+query+'&api-key='+NTY_API_KEY, function (error, response, body) {
-						
-                        parse(query,body,callback);
+			parse(query,body,callback);
 						
                 });				
 }
@@ -20,14 +18,14 @@ function parse(query,response,callback) {
 		var articles = jsonObject["response"]["docs"];
 			
 		for (i = 0; i < articles.length; i++) {
-            parsedArticle = {
-                "url": articles[i]['web_url'],
-                "source": articles[i]['source'],
-                "headline": articles[i]['headline']['main'],
-                "snippet": articles[i]['snippet'],
-                "pub_date": articles[i]['pub_date'],
-                "section_name": articles[i]['section_name'],
-                "type_of_material": articles[i]['type_of_material'],
+            var parsedArticle = {
+                "url": articles[i].web_url,
+                "source": articles[i].source,
+                "headline": articles[i].headline.main,
+                "snippet": articles[i].snippet,
+                "pub_date": articles[i].pub_date,
+                "section_name": articles[i].section_name,
+                "type_of_material": articles[i].type_of_material,
             };
 			parsedArticles.push(parsedArticle);
         }		
